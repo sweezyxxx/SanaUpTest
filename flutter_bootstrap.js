5f9facfcd4ac293922515438a3dcf2a632428f24
sweezyxxx <temirlan1536@gmail.com>
2026-04-21 00:07:19 +0500

    Built web
    Branch: master
    Commit: e5cdea5761924966e64e24b7863d4effa6df6e9b
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "779181710" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1034694564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
